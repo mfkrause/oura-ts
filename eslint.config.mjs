3a53b25bac8cf44a3ff6d0327b12d1f01992109d
Maximilian Krause <max@kuatsu.de>
2026-01-31 13:44:12 +0100

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,82 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import eslint from '@eslint/js';
+import unicornPlugin from 'eslint-plugin-unicorn';
+import prettierConfig from 'eslint-config-prettier/flat';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default defineConfig([
+  globalIgnores(['**/.*.js', '**/node_modules/', '**/dist/', '**/*-env.d.ts', '**/*.config.cjs']),
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  prettierConfig,
+  unicornPlugin.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    settings: {
+      'import/internal-regex': '^@repo/',
+    },
+
+    rules: {
+      'no-empty-pattern': 'off',
+      'sort-imports': [
+        'error',
+        {
+          ignoreDeclarationSort: true,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          'groups': [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          'alphabetize': {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/named': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+
+      'unicorn/prefer-module': 'off',
+      'unicorn/no-nested-ternary': 'off',
+      'unicorn/prefer-top-level-await': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          replacements: {
+            props: false,
+            Props: false,
+            ref: false,
+            Ref: false,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.js?(x)', '**/*.ts?(x)'],
+  },
+]);
